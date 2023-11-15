8fa7b4ef2a79f957e607faa0833cabf1394d858b
alexrodriguez <alexcarvajal2404@gmail.com>
2023-11-14 19:37:07 -0500

    :zap: updates on hero section, theme palette, ui library

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,14 @@
 import type { Config } from 'tailwindcss'
+const {nextui} = require("@nextui-org/react");
 
 const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: 'class',
   theme: {
     extend: {
       backgroundImage: {
@@ -15,6 +18,20 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [nextui({
+    // defaultTheme: 'dark',
+    themes: {
+      light: {
+        colors: {
+          background: "#f1f5f9"
+        }
+      },
+      dark: {
+        colors: {
+          background: "#0f172a"
+        }
+      }
+    }
+  })],
 }
 export default config
